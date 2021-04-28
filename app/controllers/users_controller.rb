ccf20b47c1450938bdab8bc157a0c0a83fba0533
HiDEAKI.KOIKE <canilpan779@gmail.com>
2021-04-28 06:14:49 +0000

    signup redirect change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show, :likes]
+  
+  
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(id: :desc).page(params[:page])
